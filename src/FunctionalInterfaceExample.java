74bea64162cc9a13f56fcf17dacf326f4dbddc5a
heenanitt <heenanitt@gmail.com>
2018-07-15 12:13:20 +0530

    FunctionalInterface chaining and compose example

--- src/FunctionalInterfaceExample.java
+++ src/FunctionalInterfaceExample.java
@@ -12,13 +12,28 @@ public class FunctionalInterfaceExample {
                     case 0: return "zero";
                     case 1: return "one";
                     case 2: return "two";
-                    default: return "default";
+                    default: return "default number found";
                 }
             }
         };
-
         String text = numberToText.apply(2);
         System.out.println("Number to text: " + text);
+
+        Function<String, Integer> wordCountFunction = new Function<String, Integer>() {
+            @Override
+            public Integer apply(String s) {
+                return s.split(" ").length;
+            }
+        };
+
+        //Functional Interface chaining example
+        String stringOutput = wordCountFunction.andThen(numberToText).apply("Whats this");
+        System.out.println("stringOutput: " + stringOutput);
+
+        //Functional Interface compose example
+        Integer integerOutput = wordCountFunction.compose(numberToText).apply(4);
+        System.out.println("integerOutput: " + integerOutput);
+
     }
 
 }
